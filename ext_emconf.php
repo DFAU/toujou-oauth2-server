5f1ba22fb628d1d931dfe3b62cad8bf665907497
Thomas Maroschik <tmaroschik@dfau.de>
2019-07-17 10:54:19 +0200

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'toujou Oauth2 Server',
+    'description' => '',
+    'category' => 'services',
+    'version' => '0.0.1',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'clearcacheonload' => 1,
+    'author' => 'Thomas Maroschik',
+    'author_email' => 'tmaroschik@dfau.de',
+    'author_company' => 'DFAU',
+    'constraints' => [
+        'depends' => [
+            'toujou' => '0.0.1-0.0.0'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ]
+];
+
+
