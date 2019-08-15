41223fd39d623726d8702a293819c7130149b33b
Thomas Maroschik <tmaroschik@dfau.de>
2019-08-15 21:17:41 +0200

    [TASK] Fix a http header when apache rewrites go wrong for apache server reasons…

---
+++ Classes/Middleware/AuthorizationHeaderFixer.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+
+namespace DFAU\ToujouOauth2Server\Middleware;
+
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Fixes situations with rewrite rules in apache when HTTP Headers get a REDIRECT_ prefix
+ */
+class AuthorizationHeaderFixer implements MiddlewareInterface
+{
+
+    /**
+     * Process an incoming server request.
+     *
+     * Processes an incoming server request in order to produce a response.
+     * If unable to produce the response itself, it may delegate to the provided
+     * request handler to do so.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (!$request->hasHeader('authorization') && !empty($_SERVER["REDIRECT_HTTP_AUTHORIZATION"])) {
+            $request = $request->withAddedHeader('Authorization', $_SERVER["REDIRECT_HTTP_AUTHORIZATION"]);
+        }
+        return $handler->handle($request);
+    }
+}
--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -9,6 +9,10 @@ return [
         'middlewares/payload/json-payload' => [
             'target' => \Middlewares\JsonPayload::class
         ],
+        'dfau/toujou-oauth2-server/authorization-header-fixer' => [
+            'target' => \DFAU\ToujouOauth2Server\Middleware\AuthorizationHeaderFixer::class,
+            'before' => ['dfau/toujou-oauth2-server/authorization-server']
+        ],
         'dfau/toujou-oauth2-server/authorization-server' => [
             'target' => \DFAU\ToujouOauth2Server\Middleware\AuthorizationServerMiddleware::class,
             'after' => ['typo3/cms-frontend/site', 'middlewares/payload/json-payload'],
